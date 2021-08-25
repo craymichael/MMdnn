3a37072360b7cfa1d6f131e66f934d1797e904c5
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2021-08-25 13:39:02 -0400

    tf.keras supports Functional

--- mmdnn/conversion/tf_keras/tf_keras2_graph.py
+++ mmdnn/conversion/tf_keras/tf_keras2_graph.py
@@ -1,7 +1,7 @@
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License. See License.txt in the project root for license information.
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 import os
 import tensorflow.keras as _keras
 from tensorflow import nest
@@ -13,32 +13,30 @@ class TFKeras2GraphNode(GraphNode):
     def __init__(self, layer):
         super(TFKeras2GraphNode, self).__init__(layer)
 
-
     @property
     def name(self):
         return self.layer.name
 
-
     @property
     def type(self):
         return self.layer.__class__.__name__
-
 
     @property
     def keras_layer(self):
         return self.layer
 
 
-
 class TFKeras2Graph(Graph):
 
     def __init__(self, model):
         # sanity check.
-        if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
-            raise TypeError("Keras layer of type %s is not supported." % type(model))
+        # if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
+        if not isinstance(model,
+                          (_keras.models.Sequential, _keras.models.Model)):
+            raise TypeError(
+                "Keras layer of type %s is not supported." % type(model))
         super(TFKeras2Graph, self).__init__(model)
         self.model = model
-
 
     def build(self):
         self.input_layers = list()
